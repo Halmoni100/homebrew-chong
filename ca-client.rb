af8f20bf555fb0cfed2b0048fbbc09a39609d4d8
Christopher Hong <chrishong294@gmail.com>
2023-03-10 14:10:41 -0500

    Debugged formula

--- ca-client.rb
+++ ca-client.rb
@@ -1,11 +1,12 @@
 class CaClient < Formula
   desc "SSH Certificate Helper Functions for chong IT"
   homepage ""
-  url "https://storage.googleapis.com/12add620-b983-4c5b-aa95-0a89b5c645ca/brew/ca-client_0.4.tar"
-  sha256 "487f2e4a90b40a0462ac75a1c68ad9e5558d9ddfd019850c3ea279e457e9f032"
+  url "https://storage.googleapis.com/12add620-b983-4c5b-aa95-0a89b5c645ca/brew/ca-client.tar"
+  sha256 "d5fc713b55faa46efbc8637aa18927b789e7357d5902cd6639eab0a3ff7b866b"
   license ""
-  version "0.4"
+  version "0.5"
 
+  depends_on "python3"
   depends_on "rsync"
 
   def install
