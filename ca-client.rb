4e837004487f74463890d75067d7fa08a09f8014
Christopher Hong <chrishong294@gmail.com>
2023-03-10 14:55:38 -0500

    fix

--- ca-client.rb
+++ ca-client.rb
@@ -19,6 +19,7 @@ class CaClient < Formula
     bin.install 'bin/bootstrap-step'
     bin.install 'bin/configure-ssh-client'
     bin.install 'bin/ssh-client-verify'
+    system 'mkdir', '-p', "#{prefix}/share/ca-client"
     system 'cp', 'share/ca-url', "#{prefix}/share/ca-client/ca-url"
     system 'cp', 'share/root_ca.crt', "#{prefix}/share/ca-client/root_ca.crt"
     system 'cp', 'share/ssh_host_ca_key.pub', "#{prefix}/share/ca-client/ssh_host_ca_key.pub"
