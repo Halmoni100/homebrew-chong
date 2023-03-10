2274cd8fabc323af0ed856d60b08a474a156de73
Christopher Hong <chrishong294@gmail.com>
2023-03-10 12:27:41 -0500

    updated formula

--- ca-client.rb
+++ ca-client.rb
@@ -1,22 +1,24 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class CaClient < Formula
-  desc ""
+  desc "SSH Certificate Helper Functions for chong IT"
   homepage ""
-  url "https://storage.googleapis.com/12add620-b983-4c5b-aa95-0a89b5c645ca/brew-ca-client-0.2.tar"
-  sha256 "948bebc9fc9a06708bbc8472b89efe674e75c91ef2dba64d7feac5935076b60d"
+  url "https://storage.googleapis.com/12add620-b983-4c5b-aa95-0a89b5c645ca/brew/ca-client_0.4.tar"
+  sha256 "1027baf277452ec3921bf3d674683fa87244e87f0d0cc092b486345154cdf97b"
   license ""
-  version "0.3"
+  version "0.4"
+
+  depends_on "rsync"
 
   def install
-    bin.install 'step'
-    bin.install 'configure-ssh-client'
-    bin.install 'ssh-client-verify'
+    on_arm do
+      bin.install 'bin/step_armr64' => "step"
+    end
+    on_intel do
+      bin.install 'bin/step_amd64' => "step"
+    end
+    system "./brew_install.sh"
   end
 
   test do
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "$#{bin}/step", "--help"
   end
 end
