4956fc24d1c71a740e7786a602b2a92d2d1f6029
Christopher Hong <chrishong294@gmail.com>
2023-03-10 12:37:06 -0500

    Fix tar file

--- ca-client.rb
+++ ca-client.rb
@@ -2,7 +2,7 @@ class CaClient < Formula
   desc "SSH Certificate Helper Functions for chong IT"
   homepage ""
   url "https://storage.googleapis.com/12add620-b983-4c5b-aa95-0a89b5c645ca/brew/ca-client_0.4.tar"
-  sha256 "1027baf277452ec3921bf3d674683fa87244e87f0d0cc092b486345154cdf97b"
+  sha256 "487f2e4a90b40a0462ac75a1c68ad9e5558d9ddfd019850c3ea279e457e9f032"
   license ""
   version "0.4"
 
