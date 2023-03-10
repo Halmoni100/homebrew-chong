bebee1bacdac6a4444c474a8b4ff9cde88d76d94
Christopher Hong <chrishong294@gmail.com>
2023-03-09 21:50:36 -0500

    change?

--- ca-client@1.rb
+++ ca-client@1.rb
@@ -1,7 +1,7 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class CaClient < Formula
+class CaClientAT1 < Formula
   desc ""
   homepage ""
   url "https://storage.googleapis.com/12add620-b983-4c5b-aa95-0a89b5c645ca/brew-ca-client_0.2-0-arm64.tar"
