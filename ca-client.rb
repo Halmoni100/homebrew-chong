0a402d9db1659efcd78cd0658ef1b72ae811f37d
Christopher Hong <chrishong294@gmail.com>
2023-03-10 14:24:27 -0500

    Don't use external script, manually install files

--- ca-client.rb
+++ ca-client.rb
@@ -4,7 +4,7 @@ class CaClient < Formula
   url "https://storage.googleapis.com/12add620-b983-4c5b-aa95-0a89b5c645ca/brew/ca-client.tar"
   sha256 "155f7b7c69820eb0eb9dac2e16cab30df6430c53992302ccda6d413f25dd2a23"
   license ""
-  version "0.5"
+  version "0.6"
 
   depends_on "python3"
   depends_on "rsync"
@@ -16,7 +16,11 @@ class CaClient < Formula
     on_intel do
       bin.install 'bin/step_amd64' => "step"
     end
-    system "./brew_install.sh", "#{prefix}"
+    bin.install 'bin/bootstrap-step'
+    bin.install 'bin/configure-ssh-client'
+    bin.install 'bin/ssh-client-verify'
+    share.install 'share/root_ca.crt'
+    share.install 'share/ssh_host_ca_key.pub'
   end
 
   test do
