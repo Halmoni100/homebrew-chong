2086dbf4799973aaf25b463c78f63f8724eaf5c5
Christopher Hong <chrishong294@gmail.com>
2023-03-10 15:05:45 -0500

    add binary to check cert

--- ca-client.rb
+++ ca-client.rb
@@ -2,9 +2,9 @@ class CaClient < Formula
   desc "SSH Certificate Helper Functions for chong IT"
   homepage ""
   url "https://storage.googleapis.com/12add620-b983-4c5b-aa95-0a89b5c645ca/brew/ca-client.tar"
-  sha256 "38802d4b8d26f109895c8d974ce2429c420b71330267589c373d813ce83497ce"
+  sha256 "a7e2f7e6f56659e0e8d0c93ff51b2b0d5708e1b5b87485df8ebcba33309a5518"
   license ""
-  version "0.9"
+  version "0.10"
 
   depends_on "python3"
   depends_on "rsync"
@@ -19,6 +19,7 @@ class CaClient < Formula
     bin.install 'bin/bootstrap-step'
     bin.install 'bin/configure-ssh-client'
     bin.install 'bin/ssh-client-verify'
+    bin.install 'bin/check-cert'
     system 'mkdir', '-p', "#{prefix}/share/ca-client"
     system 'cp', 'share/ca-url', "#{prefix}/share/ca-client/ca-url"
     system 'cp', 'share/root_ca.crt', "#{prefix}/share/ca-client/root_ca.crt"
