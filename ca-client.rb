4eb9b8727120fc792e62d4252ba235a05637d894
Christopher Hong <chrishong294@gmail.com>
2024-01-26 20:51:44 -0500

    update

--- ca-client.rb
+++ ca-client.rb
@@ -2,9 +2,9 @@ class CaClient < Formula
   desc "SSH Certificate Helper Functions for chong IT"
   homepage ""
   url "https://storage.googleapis.com/12add620-b983-4c5b-aa95-0a89b5c645ca/brew/ca-client.tar"
-  sha256 "f2005b1699a781663cff33d5ceb4bf53284f2449cfebfecff6abe7d7e20dba8f"
+  sha256 "9c629957ed48903a08bfb3efa3a0149eefe8ccc13759a49f4e60f71323882197"
   license ""
-  version "0.9"
+  version "0.10"
 
   depends_on "python3"
   depends_on "rsync"
