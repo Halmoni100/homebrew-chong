7d238c7e078c4e5f7ea32bbdbbf170d7a9414b87
Ubuntu <chrishong294@gmail.com>
2024-08-25 23:47:46 -0400

    update version

--- chong-helper.rb
+++ chong-helper.rb
@@ -2,9 +2,9 @@ class ChongHelper < Formula
   desc "Helper scripts"
   homepage ""
   url "https://storage.googleapis.com/12add620-b983-4c5b-aa95-0a89b5c645ca/brew/chong-helper.tar"
-  sha256 "17c4935d378bd5796458b0257132c21445528365cca0bac00efdbeedc8531599"
+  sha256 "73cef08c9871bf6e5861679b784e111e1b51b90be4f37071b6d2293cf5d79b06"
   license ""
-  version "0.2"
+  version "0.3"
 
 
 
