d3913d065db901086df6c0f30b3d0bd1de4b60e3
Christopher Hong <chrishong294@gmail.com>
2024-10-15 14:12:30 -0400

    update chong-helper

--- chong-helper.rb
+++ chong-helper.rb
@@ -2,9 +2,9 @@ class ChongHelper < Formula
   desc "Helper scripts"
   homepage ""
   url "https://storage.googleapis.com/12add620-b983-4c5b-aa95-0a89b5c645ca/brew/chong-helper.tar"
-  sha256 "3fe020c507fbdf0baed80c4f652e7b88e7ba721da63140fa254a105f9c047239"
+  sha256 "d30a3f9c64b5a3019e271265fae09cde2fc4638fba50bff441a2d68be29e378c"
   license ""
-  version "0.3"
+  version "0.4"
 
 
 
